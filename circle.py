ba7232b82c3689c741c97a6c7053e867addb74a7
monad <agorist13@protonmail.com>
2020-09-26 15:00:21 +0700

    Started project.

---
+++ circle.py
@@ -0,0 +1,8 @@
+
+import circleCalc
+
+def main():
+    print('circle area with radius 5:', circleCalc.circleArea(5))
+    print('circumference with radius 5:', circleCalc.circleCircumference(5))
+
+main()
